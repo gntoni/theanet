937cabf69530df34faf6296e4e67e7434df55a7f
Toni Gabas <agabas@iri103.(none)>
2015-04-24 18:25:28 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='TheaNet',
+      version='1.0',
+      description='Utilities to create a deep neural network with theano',
+      author='Toni Gabas',
+      author_email='agabas@iri.upc.edu',
+      url='https://github.com/gntoni/theanet',
+      packages=['theanet'],
+     )
